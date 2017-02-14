21a8366cb7ac3a0e4b4dbdc9aa51bd63044bc9a1
SENON, Julien - NG27990 <julien.senon.external@airbus.com>
2017-02-14 17:32:12 +0100

    go

--- src/stringutil/stringutil.go
+++ src/stringutil/stringutil.go
@@ -1,4 +1,4 @@
-// Package stringutil contains utility functions for working with strings.
+rm pk	// Package stringutil contains utility functions for working with strings.
 package stringutil
 
 // Reverse returns its argument string reversed rune-wise left to right.
