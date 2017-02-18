a10c2d354c84ef21a564c2d81993b1c9f9201bf8
Julien SENON <julien@JulienMBP.local>
2017-02-18 17:53:58 +0100

    array exercise

--- src/mylearn/mylearn.go
+++ src/mylearn/mylearn.go
@@ -4,9 +4,20 @@ import (
 	"fmt"
 )
 
-func main() {
-	total := [5]float64{1, 2, 3, 4, 5}
-	for i := 0; i < len(total); i++ {
-		fmt.Printf("%f\n", total[i])
+/*func main() {
+	array := [5]float64{1, 2, 3, 4, 5}
+	for i := 0; i < len(array); i++ {
+		fmt.Printf("%f\n", array[i])
 	}
+}
+*/
+
+func main() {
+	array := [5]float64{1, 2, 3, 4, 5}
+	var total float64 = 0
+	for _, value := range array {
+		total += value
+	}
+	fmt.Println(total / float64(len(array)))
+	fmt.Printf("%f", total/float64(len(array)))
 }
