0e5e198a0081e2a6b147092177e38f1905044abf
Julien SENON <julien@JulienMBP.local>
2017-02-18 18:36:48 +0100

    slice maps exercise

--- src/mylearn/mylearn.go
+++ src/mylearn/mylearn.go
@@ -21,3 +21,16 @@ func main() {
 	fmt.Println(total / float64(len(array)))
 	fmt.Printf("%f", total/float64(len(array)))
 }
+
+
+package main
+
+import "fmt"
+
+func main() {
+	slice1 := []int {1,2,3}
+	slice2 := [] int {4,5,6,7}
+	slice3 := make ([]int,5)
+	copy (slice3, slice2)
+	fmt.Println(slice1,slice2,slice3)
+}
