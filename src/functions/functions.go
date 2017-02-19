ca82328af9d608c782d51b0d5972081240e42610
Julien SENON <julien.senon@gmail.com>
2017-02-19 18:53:57 +0100

    func factorial

--- src/functions/functions.go
+++ src/functions/functions.go
@@ -12,7 +12,20 @@ func average(xs []float64) float64 {
 	return total / float64(len(xs))
 }
 
-func main() {
-	array := []float64{1, 2, 3, 4, 5}
-	fmt.Println(average(array))
+func factorial(x uint) uint {
+	if x == 0 {
+		return 1
+	}
+	return x * factorial(x-1)
 }
+
+func main() {
+	//	array := []float64{1, 2, 3, 4, 5}
+	//	fmt.Println(average(array))
+	fmt.Println("Enter your Number: ")
+	var input uint
+	fmt.Scanf("%d", &input)
+	fmt.Println("Your Result :", factorial(input))
+}
+
+//Variadic functions wrote func Myfunc(args ...int) int {}
