f00a9aeb46a872a5265d63dca64064a91ce552c3
Julien SENON <julien@JulienMBP.local>
2017-02-19 17:13:36 +0100

    map exercise

---
+++ src/maps/maps.go
@@ -0,0 +1,23 @@
+//var x map[string]int
+// x := make(map[string]int)
+
+package main
+
+import "fmt"
+
+func main() {
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+	}
+}
