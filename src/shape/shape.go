405a369e3700d6a19a2640f8bc3d6232f24fc1fe
Julien SENON <julien.senon@gmail.com>
2017-02-26 18:05:18 +0100

    Add interace exercise

---
+++ src/shape/shape.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
+
+// Methods
+
+func totalArea(shapes ...Shape) float64 {
+	var area float64
+	for _, s := range shapes {
+		area += s.area()
+	}
+	return area
+}
+
+// ... is a variadic function, is allowed to be called with multiple value
+
+type Circle struct {
+	x, y, ra float64
+}
+
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func (c *Circle) area() float64 {
+	return math.Pi * c.ra * c.ra
+}
+
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
+
+type Shape interface {
+	area() float64
+}
+
+func main() {
+	c := Circle{0, 0, 5}
+	r := Rectangle{0, 0, 10, 10}
+	fmt.Println("Rectangle Area is:", r.area())
+	fmt.Println("Circle Area is:", c.area())
+	fmt.Println("Total Area is:", totalArea(&c, &r))
+
+}
