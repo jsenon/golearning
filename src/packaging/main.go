30211ef4af5b887110b941350c687a9d15b343e1
Julien SENON <julien.senon@gmail.com>
2017-02-27 00:05:01 +0100

    Add min max to function

--- src/packaging/main.go
+++ src/packaging/main.go
@@ -5,31 +5,11 @@ import (
 	m "packaging/math"
 )
 
-func Max(xs []float64) float64 {
-	var ma float64
-	for i, x := range xs {
-		if i == 0 || x > ma {
-			ma = x
-		}
-	}
-	return ma
-}
-
-func Min(xs []float64) float64 {
-	var mi float64
-	for i, x := range xs {
-		if i == 0 || x < mi {
-			mi = x
-		}
-	}
-	return mi
-}
-
 func main() {
 	xs := []float64{1, 2, 3, 4}
 	avg := m.Average(xs)
-	max := Max(xs)
-	min := Min(xs)
+	max := m.Max(xs)
+	min := m.Min(xs)
 	fmt.Println("Average:", avg)
 	fmt.Println("Max:", max)
 	fmt.Println("Min:", min)
--- src/packaging/math/math.go
+++ src/packaging/math/math.go
@@ -7,3 +7,23 @@ func Average(xs []float64) float64 {
 	}
 	return total / float64(len(xs))
 }
+
+func Max(xs []float64) float64 {
+	var ma float64
+	for i, x := range xs {
+		if i == 0 || x > ma {
+			ma = x
+		}
+	}
+	return ma
+}
+
+func Min(xs []float64) float64 {
+	var mi float64
+	for i, x := range xs {
+		if i == 0 || x < mi {
+			mi = x
+		}
+	}
+	return mi
+}
