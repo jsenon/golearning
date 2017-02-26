e9dc081fa8150d324880c3004d4c53d9e3354aed
Julien SENON <julien.senon@gmail.com>
2017-02-26 23:56:33 +0100

    Add min max

--- src/packaging/main.go
+++ src/packaging/main.go
@@ -5,8 +5,32 @@ import (
 	m "packaging/math"
 )
 
+func Max(xs []float64) float64 {
+	var ma float64
+	for i, x := range xs {
+		if i == 0 || x > ma {
+			ma = x
+		}
+	}
+	return ma
+}
+
+func Min(xs []float64) float64 {
+	var mi float64
+	for i, x := range xs {
+		if i == 0 || x < mi {
+			mi = x
+		}
+	}
+	return mi
+}
+
 func main() {
 	xs := []float64{1, 2, 3, 4}
 	avg := m.Average(xs)
-	fmt.Println(avg)
+	max := Max(xs)
+	min := Min(xs)
+	fmt.Println("Average:", avg)
+	fmt.Println("Max:", max)
+	fmt.Println("Min:", min)
 }
